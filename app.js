06b2ba476e2b4c032765d92d063784f94e9fcf32
Umar <umarabdullah.work@gmail.com>
2024-06-03 21:13:51 +0500

    Completed JS file and added all functions

--- app.js
+++ app.js
@@ -15,3 +15,52 @@ const winPatterns = [
   [3, 4, 5],
   [6, 7, 8],
 ];
+const resetGame= () => {
+    turnO= true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+  }
+  boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+      if (turnO) {
+        box.innerText = "O";
+        turnO = false;
+      } else {
+        box.innerText = "X";
+        turnO = true;
+      }
+      box.disabled = true;
+      checkWinner();
+    });
+  });
+  const disableBoxes = () => {
+    for(let box of boxes){
+      box.disabled = true;
+    }
+  }
+  const enableBoxes = () => {
+    for(let box of boxes){
+      box.disabled = false;
+      box.innerText = "";
+    }
+  }
+  const showWinner= (winner) => {
+      msg.innerText = `Congratulations, Winner is ${winner}`;
+      msgContainer.classList.remove("hide");
+      disableBoxes();
+  }
+  const checkWinner = () => {
+    for (let pattern of winPatterns) {
+      let pos1val= boxes[pattern[0]].innerText;
+      let pos2val= boxes[pattern[1]].innerText;
+      let pos3val= boxes[pattern[2]].innerText;
+      if(pos1val != "" && pos2val != "" && pos3val != ""){
+          if(pos1val === pos2val && pos2val === pos3val){
+              console.log("Winner", pos1val);
+              showWinner(pos1val);
+          }
+      }
+    }
+  }; 
+  resetBtn.addEventListener("click", resetGame);
+  newGameBtn.addEventListener("click", resetGame);
